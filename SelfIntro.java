1b4cb7afa56021caa9b8ad74c6ed1064426ad74f
MitoYuuki <nekoneko047@gmail.com>
2026-02-18 14:14:01 +0900

    自己紹介

---
+++ SelfIntro.java
@@ -0,0 +1,12 @@
+public class SelfIntro {
+    public static void main(String[] args) {
+        String name = "ゆうき";
+        int age = 20;
+        String hobby = "ゲームとプログラミング";
+
+        System.out.println("こんにちは！");
+        System.out.println("名前: " + name);
+        System.out.println("年齢: " + age + "歳");
+        System.out.println("趣味: " + hobby);
+    }
+}
